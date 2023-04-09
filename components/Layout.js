71aa7409448243a10b45db054f60eda44fe22b77
Eten-Ekata <noeladaidu@gmail.com>
2023-04-09 03:07:34 +0100

    keep it coming

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,31 @@
+import { Store } from '@/utils/Store'
+import { signOut, useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Link from 'next/link'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Menu } from '@headlessui/react'
+import DropdownLink from './DropdownLink'
+import Cookies from 'js-cookie'
 
 export default function Layout({ title, children }) {
+
+  const { status, data: session } = useSession()
+  const { state, dispatch } = useContext(Store)
+  const { cart } = state
+  
+  const [cartItemsCount, setCartItemsCount] = useState(0)
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a,c) => a + c.quantity, 0))
+  },[cart.cartItems])
+
+  const logoutClickHandler = () => {
+    Cookies.remove('cart')
+    dispatch({type: 'CART_RESET'})
+    signOut({callBackUrl: '/login'})
+  }
+
   return (
     <>
       <Head>
@@ -11,15 +34,58 @@ export default function Layout({ title, children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ToastContainer position='bottom-center' limit={1} />
       <div className='flex flex-col min-h-screen justify-between'>
         <header>
-          <nav className='flex h-12 justify-between items-center shadow-lg'>
+          <nav className='flex h-12 px-4 justify-between items-center shadow-md'>
             <Link href='/' legacyBehavior>
               <a className='text-lg font-bold'>amazona</a>
             </Link>
             <div>
-              <Link href='/cart' legacyBehavior><a className='p-2'>Cart</a></Link>
-              <Link href='/login' legacyBehavior><a className='p-2'>Login</a></Link>
+              <Link href='/cart' legacyBehavior>
+                <a className='p-2'>Cart
+                  {cartItemsCount > 0 && (
+                    <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white '>
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </a>
+              </Link>
+
+              {status === 'loading' ? ('Loading...'):
+                
+                  session?.user ? 
+                  <Menu as='div' className='relative inline-block'>
+                    <Menu.Button className='text-blue-600'>
+                      {session.user.name}
+                    </Menu.Button>
+                    <Menu.Items className='absolute right-0 w-56 bg-white origin-top-right shadow-lg'>
+                      <Menu.Item>
+                        <DropdownLink className='dropdown-link' href='/profile'>
+                          Profile
+                        </DropdownLink>
+                      </Menu.Item>
+                      <Menu.Item>
+                        <DropdownLink className='dropdown-link' href='/order-history'>
+                          Order History
+                        </DropdownLink>
+                      </Menu.Item>
+                      <Menu.Item> 
+                        <a className='dropdown-link' 
+                          href='#' 
+                          onClick={logoutClickHandler}>
+                            Logout 
+                        </a>
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Menu>
+                 :
+                (
+                  <Link href='/login' legacyBehavior> 
+                    <a className='p-2'>Login </a> 
+                  </Link>
+                )
+                }
             </div>
           </nav>
         </header>
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,19 +1,35 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useContext } from 'react'
 import data from '../../utils/data'
 import Layout from '../../components/Layout'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Store } from '../../utils/Store'
 
 export default function ProductScreen() {
 
+  const { state, dispatch } = useContext(Store)
   const { query } = useRouter()
   const { slug } = query
   const product = data.products.find(x => x.slug === slug)
 
+  const router = useRouter()
+
   if (!product){
     return <div>Product not found</div>
   }
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((item) => item.slug === product.slug)
+    const quantity = existItem ? existItem.quantity + 1 : 1
+
+    if (product.countInStock < quantity){
+      return alert('Sorry, Product is out of stock')
+    }
+    dispatch({type: 'CART_ADD_ITEM', payload: {...product, quantity}})
+    router.push('/cart')
+  }
+
   return (
     <Layout title={product.name}>
       <div className='py-2'>
@@ -58,7 +74,12 @@ export default function ProductScreen() {
               <div>Status</div>
               <div>{product.countInStock > 0 ? 'In Stock' : 'Unavailable' } </div>
             </div>
-            <button className='primary-button w-full' >Add To Cart</button>
+            <button 
+              className='primary-button w-full' 
+              onClick={addToCartHandler}
+            >
+              Add To Cart
+            </button>
           </div>
         </div>
       </div>
